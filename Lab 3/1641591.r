0e303828ecf9afa7b61067df5661cec654bdae1b
arenzo97 <larenas97@gmail.com>
2019-11-26 17:05:03 +0000

    finished assessment 1

--- Lab 3/1641591.r
+++ Lab 3/1641591.r
@@ -1,5 +1,5 @@
 #install.packages("rpart")
-#library(rpart)
+library(rpart)
 library(class)
 
 #Set Directory to this current directory
@@ -7,59 +7,33 @@ this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
 
 #set data file, class and values
-iris = read.csv('iris.csv', sep=",")
-irisClass = iris[,1]
-irisValues = iris[.-1]
+winedata = read.csv('winedata.csv', sep=",")
+wineclass = winedata[,1]
+winevalues = winedata[,-1]
 
-TrainingSet<-function(idataClass,idataValues,setValues)
-{
-    idataClassTrain = idataClass[1:setvalues]
-    idataValuesTrain = idataValues[1:setvalues]
-}
+#set up a training set
+wineclassTrain = wineclass[1:100]
+winevaluesTrain = winevalues[1:100,]
+#and testset
+wineclassTest = wineclass[101:178]
+winevaluesTest = winevalues[101:178,]
 
-#DecisionTree
-KNN
+fit <- rpart(wineclassTrain~., method="class", data=winevaluesTrain)
 
-#Decision trees
-DecisionTree<-function()
-{
-    fit <- rpart(wineclassTrain~., method = "class", data=winevaluesTrain)
-    plot(fit, uniform=TRUE, main="Decision Tree for WineData")
-    text(fit, use.n=TRUE, all=TRUE, cex=.8)
-    
-    #calculate predictions for testcases
-    treepred<-predict(fit,winevaluesTest,type='class')
+plot(fit, uniform=TRUE, main="Decision Tree for WineData3")
+text(fit, use.n=TRUE, all=TRUE, cex=.8)
 
-    #return accuracy
-    AccuracyFunc(length(wineclassTest),sum(treepred==wineclassTest))
+treepred <-predict(fit, winevaluesTest, type = 'class')
 
-    #confusion matrix results
-    table_mat=table(wineclassTest,treepred)
-    print(table_mat)
-}
+n = length(wineclassTest) #the number of test cases
+ncorrect = sum(treepred==wineclassTest) #the number of correctly
+predicted
+accuracy=ncorrect/n
+print(accuracy)
 
-Prune<-function(fit,pruneValue)
-{
-    #pruning
-    pfit<-prune(fit,cp=pruneValue)
-    plot(pfit,uniform=TRUE,main="Pruned Decision Tree for WineData")
-    text(pfit,use.n=TRUE, all=TRUE,cex=.8)
-}
-#KNN
-KNN<-function()
-{
-    knn3pred = knn(winevaluesTrain, winevaluesTest, wineclassTrain, k=3)
+table_mat = table(wineclassTest, treepred)
+print(table_mat)
 
-    #calculate accuracy
-    n=length(wineclassTest)
-    ncorrect=sum(knn3pred==wineclassTest)
-    AccuracyFunc(ncorrect,n)
-
-}
-
-
-AccuracyFunc<-function(ncorrect,n)
-{
-    accuracy=ncorrect/n
-    print(accuracy)
-}
+pfit<- prune(fit, cp=0.1)
+plot(pfit, uniform=TRUE, main="Pruned Decision Tree for WineData3")
+text(pfit, use.n=TRUE, all=TRUE, cex=.8)
--- Lab 3/1641591_1.r
+++ Lab 3/1641591_1.r
@@ -1,28 +1,22 @@
 #install.packages("rpart")
 library(rpart)
+library(rpart.plot)
 library(class)
 
 #Set Directory to this current directory
 this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
 
+splitScreen <- function(x ,y){
+    par(mfrow=c(y,x))
+}
+
+
 #set data file, class and values
-iris = read.csv('iris.csv', sep=",")
+iris = read.csv('iris.csv', head = FALSE,sep=",")
+irisReal = read.csv('iris_real.csv',head = FALSE, sep=",")
 
-
-TrainingSet<-function(idataClass,idataValues)
-{
-    idataClassTrain = idataClass[1:100]
-    idataValuesTrain = idataValues[1:100,]
-    result <- list(classTrain = idataClassTrain, classValues = idataClassTrain)
-    return(result)
-}
-
-TestSet<-function(idataClass,idataValues,setValues)
-{
-    idataClassTest = idataClass[setValues]
-    idataValuesTest= idataValues[setValues,]
-}
+# returns a list
 
 
 #KNN Function
@@ -33,28 +27,21 @@ KNN<-function(valuesTrain,valuesTest,classTrain,kvalue)
 }
 
 #Decision trees
-DecisionTree<-function(trainSet,methodType,dataset)
-{
-    
-    plot(fit, uniform=TRUE, main="Decision Tree for",trainSet)
-    text(fit, use.n=TRUE, all=TRUE, cex=.8)
 
-    return(fit)
+
+PlotTree <- function(fit, type, title)
+{
+    rpart.plot(x = fit, type = type, main = title, box.palette = list("#552586","#804FB3","#B589D6"))  
 }
 
-Prune<-function(fit,pruneValue)
-{
-    #pruning
-    pfit<-prune(fit,cp=pruneValue)
-    plot(pfit,uniform=TRUE,main="Pruned Decision Tree for WineData")
-    text(pfit,use.n=TRUE, all=TRUE,cex=.8)
-}
-#calculate accuracy
-Accuracy<-function(knnGenValues,testValues)
+# Confusion Matr
+
+# Calculate accuracy
+Accuracy<-function(actualValues,testValues)
 {
     
     n=length(testValues)
-    ncorrect=sum(knnTestedValues==testValues)
+    ncorrect=sum(actualValues==testValues)
     result = ncorrect/n
     
     print(result)
@@ -62,12 +49,69 @@ Accuracy<-function(knnGenValues,testValues)
 
 }
 
-
-iris_rand=iris[sample(150,150),]
+#1 Create Decision Tree for Iris Classes
+iris_rand<-cbind(irisReal,iris)
+iris_rand=iris_rand[sample(149,149),]
 irisClass = iris_rand[,1]
 irisValues = iris_rand[,-1]
 
+irisClassTrain = irisClass[1:100]
+irisValuesTrain = irisValues[1:100,]
 
-irisTrain1=TrainingSet(irisClass,irisValues)
-irisClassTrain = irisTrain1[1]
-# irisTrain1fit <- rpart(irisClassTrain~., method = "class", data=irisTrain1[2])
+irisClassTest = irisClass[101:149]
+irisValuesTest = irisValues[101:149,]
+
+
+
+irisTrainfit <- rpart(irisClassTrain~., method = "class", data=irisValuesTrain)
+treepred <- predict(irisTrainfit,irisValuesTest, type = 'class')
+rpart.plot(x = irisTrainfit, type = 4, main = "Decision Tree for IrisData",
+           box.palette = list("#325aa8","#325ee8","#325ae8"))  
+
+
+
+#2 Test accuracy by pruning
+splitScreen(2, 3)
+cpvalues <- c(0.04, 0.08, 0.1,0.4,0.5)
+cpLength = length(cpvalues)
+
+prunedTreesAccuracies <- vector( mode = "numeric", length = cpLength)
+prunedTreesFitValues<-list()
+
+for (i in 1:5)
+{
+    prunedTree<-prune(irisTrainfit,cp=cpvalues[i])
+    PlotTree(prunedTree, 4,sprintf('Decision tree with cp = %g', cpvalues[i]))
+    prunedTreesAccuracies[i]=Accuracy(predict(prunedTree,irisValuesTest,type = 'class'),irisClassTest)
+    
+}
+
+#3 Scatter plot 2 selected variables and colour code based on decision trees
+
+splitScreen(3,1)
+plot(x = irisValuesTrain$V1, y = irisValuesTrain$V2, col = c(irisClassTrain), main = "ValuesTrain Values of V1 vs V2")  
+plot(x = irisValuesTest$V1, y = irisValuesTest$V2,
+     col = c(irisClassTest), main = "Test values")
+plot(x = irisValuesTest$V1, y = irisValuesTest$V2,
+     col = c(treepred), main = "Predicted values")
+
+# 4. Compare the accuracy of the different pruned trees to KNN with different values of k ####
+
+rep = 10
+
+knnAccuracies <- vector( mode = "numeric", length = rep)
+KValues <- vector( mode = "integer", length = rep)
+
+#Print Accuracy values of KNN and Pruned Trees using different values of k
+for (i in 1 : rep) {
+    
+    knnPrediction = knn(irisValuesTrain, irisValuesTest, irisClassTrain, k = i)
+    knnAccuracies[i] = Accuracy(knnPrediction, irisClassTest)
+    KValues[i] = i
+}
+
+df = data.frame("K" = KValues, "KNN Accuracies" = knnAccuracies,
+                "CP 0.04" = prunedTreesAccuracies[1], "CP 0.08" = prunedTreesAccuracies[2], "CP 0.1" = prunedTreesAccuracies[3], "CP 0.5" = prunedTreesAccuracies[4],"CP 0.6" = prunedTreesAccuracies[5]) 
+
+print(df)
+
